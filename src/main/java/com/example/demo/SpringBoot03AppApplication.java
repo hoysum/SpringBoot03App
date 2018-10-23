cdc286d92aa4f961b3c4cea42d5807e9b9e8bddf
GBTC406005UR <GBTC406005UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-10-23 14:18:11 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/SpringBoot03AppApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot03AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot03AppApplication.class, args);
+	}
+}
